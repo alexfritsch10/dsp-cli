2f03489af6f9ad4832817bf708fcd5d9d8633bc7
Alexander Fritsch <alexfritsch10@gmail.com>
2021-01-06 13:21:13 +0100

    Clearify error messages

--- mercury/validateJSONSchema.py
+++ mercury/validateJSONSchema.py
@@ -19,15 +19,15 @@ def validateJSONSchema(obj):
         
         if value.get("Type") == "FReD":
             if not type(value.get("Properties")) == dict:
-                return generateResponse("Propeties Attribute of Resource should be an Object")
+                return generateResponse("'Propeties' attribute of Resource should be an Object or is not found")
             
 
             if not type(value.get("Properties").get("Nodes")) == dict:
-                return generateResponse("Nodes Attribute of Propeties should be an Object")
+                return generateResponse("'Nodes' attribute of Propeties should be an Object or is not found")
             
 
             if not type(value.get("Properties").get("Nodes").get("NodeNames")) == list:
-                return generateResponse("NodeNames Attribute of Nodes should be an Object")
+                return generateResponse("'NodeNames' attribute of Nodes should be an Object or is not found")
             
             
                 
@@ -40,7 +40,7 @@ def validateJSONSchema(obj):
             
 
             if not type(value.get("Properties").get("Clients")) == list:
-                return generateResponse("Clients Attribute of Properties should be an Object")
+                return generateResponse("'Clients' attribute of Properties should be an Object or is not found")
             
 
             for clientValue in value.get("Properties").get("Clients"):
@@ -48,52 +48,49 @@ def validateJSONSchema(obj):
                     return generateResponse("Client name should be a string")
 
             if not type(value.get("Properties").get("KeyGroups")) == list:
-                return generateResponse("KeyGroups Attribute of Properties should be an Object")
+                return generateResponse("'KeyGroups' attribute of Properties should be an Object or is not found")
 
             for keyGroupValue in value.get("Properties").get("KeyGroups"):
                 if not type(keyGroupValue.get("Name")) == str:
-                    return generateResponse("KeyGroup name should be a string")
+                    return generateResponse("KeyGroup 'Name' should be a string or is not found")
                 
                 if not re.compile("^[a-zA-Z0-9]+$").match(keyGroupValue.get("Name")):
-                    return generateResponse("Keygroup name does not match ^[a-zA-Z0-9]+$")
+                    return generateResponse("Keygroup 'Name' does not match ^[a-zA-Z0-9]+$")
                 
                 if len(list(filter(lambda keygroup: keygroup.get("Name") == keyGroupValue.get("Name"), value.get("Properties").get("KeyGroups")))) > 1:
                     return generateResponse("KeyGroup names can only be given once")
                 
                 if not type(keyGroupValue.get("Mutable")) == bool:
-                    return generateResponse("KeyGroup attribute mutable should be a boolean")
-                
-                if not type(keyGroupValue.get("Expiry")) == int:
-                    return generateResponse("KeyGroup attribute expiry should be a integer")
+                    return generateResponse("KeyGroup attribute 'Mutable' should be a boolean or is not found")
                 
                 if not type(keyGroupValue.get("Replicas")) == list:
-                    return generateResponse("KeyGroup attribute Replicas should be a object")
+                    return generateResponse("KeyGroup attribute 'Replicas' should be a object or is not found")
                 
 
                 for repValue in keyGroupValue.get("Replicas"):
                     if not type(repValue.get("Name")) == str:
-                        return generateResponse("KeyGroup Replica name should be a string")
+                        return generateResponse("KeyGroup replica 'Name' should be a string or is not found")
                     
                     if not repValue.get("Name") in value.get("Properties").get("Nodes").get("NodeNames"):
-                        return generateResponse("KeyGroup Replica name should be in the Nodes List")
+                        return generateResponse("KeyGroup replica Name should be in the nodes list")
                     
                     if len(list(filter(lambda rep: rep.get("Name") == repValue.get("Name"), keyGroupValue.get("Replicas")))) > 1:
-                        return generateResponse("KeyGroup Replica can only appear once in the list")
+                        return generateResponse("KeyGroup replica can only appear once in the list")
                     
                     if not type(repValue.get("Expiry")) == int:
-                        return generateResponse("KeyGroup Replica attribute expiry should be a integer")
+                        return generateResponse("KeyGroup replica attribute 'Expiry' should be a integer or is not found")
                     
 
                 if not type(keyGroupValue.get("Triggers")) == list:
-                    return generateResponse("KeyGroup attribute Replicas should be a object")
+                    return generateResponse("KeyGroup attribute 'Triggers' should be a list or is not found")
                 
 
                 for triValue in keyGroupValue.get("Triggers"):
                     if not type(triValue.get("Name")) == str:
-                        return generateResponse("KeyGroup Replica name should be a string")
+                        return generateResponse("KeyGroup trigger 'Name' should be a string or is not found")
 
                     if not type(triValue.get("ReceiverAddress")) == int:
-                        return generateResponse("KeyGroup Trigger attribute expiry should be a integer")
+                        return generateResponse("KeyGroup trigger attribute expiry should be a integer")
 
     return generateResponse("Schema contraints fulfilled", True)
 
